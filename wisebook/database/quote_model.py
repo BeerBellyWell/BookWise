029d391d2c290c1daa4f002c0397ad5b7cc18705
Андрей Чернышев <candreey17@mai.ru>
2024-03-06 17:04:41 +1200

    add form

--- wisebook/database/quote_model.py
+++ wisebook/database/quote_model.py
@@ -3,4 +3,11 @@ from wisebook import db
 
 class Quote(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    text = db.Column(db.String(256), nullable=False)
+    quote = db.Column(db.String(256), nullable=False)
+    book_name = db.Column(db.String(128), nullable=False)
+    book_author = db.Column(db.String(128), nullable=False)
+    create_at = db.Column(db.DateTime, default=db.func.current_timestamp())
+    like = db.Column(db.Integer, default=0, nullable=False)
+    # last_update = db.Column(db.DateTime, onupdate=db.func.current_timestamp())
+    # is_published = db.Column(db.Boolean, default=True)
+
