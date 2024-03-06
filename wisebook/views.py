3df10dd1bf9b8dbe66517cd8061b347cffa4387a
Андрей Чернышев <candreey17@mail.ru>
2024-03-06 22:03:42 +1200

    form, html, view

--- wisebook/views.py
+++ wisebook/views.py
@@ -1,18 +1,28 @@
-from flask import render_template
+from flask import render_template, redirect, url_for
 from wisebook.database.quote_model import Quote
-from wisebook import app
+from wisebook import app, db
 from wisebook.forms import QuoteForm
 
 
 @app.route('/')
-def hello():
-    # quotes = Quote.query.all()
-    return render_template('pages/index.html') #quotes=quotes)
+def index():
+    quotes = Quote.query.all()
+    return render_template('pages/index.html', quotes=quotes)
 
 
-@app.route('/add_quote')
+@app.route('/add_quote', methods=['GET', 'POST'])
 def add_quote():
     form = QuoteForm()
+    if form.validate_on_submit():
+        quote = Quote(
+            quote = form.quote.data,
+            book_name = form.book_name.data,
+            book_author = form.book_author.data
+        )
+        
+        db.session.add(quote)
+        db.session.commit()
+        return redirect(url_for('index'))
     return render_template('pages/add_quote.html', form=form)
 
 
