bbd87145db2f302fe648b8a69c9b1fa77dcb7be0
Андрей Чернышев <candreey17@mail.ru>
2024-03-06 20:12:19 +1200

    little changes

--- wisebook/views.py
+++ wisebook/views.py
@@ -7,10 +7,15 @@ from wisebook.forms import QuoteForm
 @app.route('/')
 def hello():
     # quotes = Quote.query.all()
-    return render_template('index.html') #quotes=quotes)
+    return render_template('pages/index.html') #quotes=quotes)
 
 
 @app.route('/add_quote')
 def add_quote():
     form = QuoteForm()
-    return render_template('add_quote.html', form=form)
+    return render_template('pages/add_quote.html', form=form)
+
+
+@app.route('/test')
+def test_html():
+    return render_template('test.html')
